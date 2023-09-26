2b68946ae64b6702fc787a0f56feba4145a95333
cpineda <carlosepjorge@gmail.com>
2023-09-25 23:38:41 -0400

    handling the cancellation button and displaying information after registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AccountService } from 'src/services/account.service';
 import { User } from './models/user';
 
@@ -10,23 +9,11 @@ import { User } from './models/user';
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users: any;
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {}
+
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7067/Users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
   }
 
   setCurrentUser() {
