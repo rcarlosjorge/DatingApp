137ae4b12013e5e12a67cd6f6864bae1a6b98210
cpineda <carlosepjorge@gmail.com>
2023-09-30 09:52:20 -0400

    route guard and share module added

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/services/account.service';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+  return accountService.currentUser$.pipe(
+    map((auth) => {
+      if (auth) return true;
+      toastr.error('You shall not pass!');
+      return false;
+    })
+  );
+};
